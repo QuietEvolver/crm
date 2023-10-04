0fd69cc7edac5302b447763c3c36467c47cfe4d1
VW <vera.weikel@gmail.com>
2023-10-04 15:36:46 -0700

    Update resolve syntax.

--- data/resolvers.js
+++ data/resolvers.js
@@ -6,13 +6,13 @@ const resolvers = {
     getProduct: ({id}) => {
         return new Promise(( resolve ) => Widgets.findById({_id: id}), (err, product) => {
             if (err) reject(err)
-            else resolvers(product)
+            else resolve(product)
         });
     },
     createProduct: ({ input }) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        productDatabase[id] = input;
-        return new Product(id, input);
+        // let id = require('crypto').randomBytes(10).toString('hex');
+        // productDatabase[id] = input;
+        // return new Product(id, input);
     }
 }
 
