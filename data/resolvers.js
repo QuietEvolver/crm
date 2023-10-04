ebf04de9b591bb1656baa7b018dda50623dd985c
VW <vera.weikel@gmail.com>
2023-10-04 15:33:49 -0700

    Update resolver to connect to mongoose and get the product.

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,22 +1,13 @@
-// mutation: constructor
-class Product {
-    constructor(id, { name, description, price, soldout, inventory, stores }) {
-        this.id = name;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.soldout = soldout;
-        this.inventory = inventory;
-        this.stores = stores;
-    }
-}
+import { reject } from 'lodash';
+import { Widgets } from '../dbConnectors';
 
-// mutation setup: database
-const productDatabase = {};
 
 const resolvers = {
-    getProducts: ({id}) => {
-        return new Product(id, productDatabase[id]);
+    getProduct: ({id}) => {
+        return new Promise(( resolve ) => Widgets.findById({_id: id}), (err, product) => {
+            if (err) reject(err)
+            else resolvers(product)
+        });
     },
     createProduct: ({ input }) => {
         let id = require('crypto').randomBytes(10).toString('hex');
