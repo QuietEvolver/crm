3379e8818f14824fc8faaeda81df74b29d5366a6
VW <vera.weikel@gmail.com>
2023-10-04 08:36:34 -0700

    Make app express get server to listen on localhost endpoint.

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send(`GraphQL is an amazing query language for your API`);
+});
+
+// Add port server listens to
+app.listen(3000, () => console.log('Running server on port localhost:8080/graphql'));
+
