629c7ccb787ea4f31bd27d2399db7136c2ff9272
VW <vera.weikel@gmail.com>
2023-10-04 09:56:00 -0700

    Create a response from graphiql.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send(`GraphQL is an amazing query language for your API`);
 });
+const root = { hello: () => "Hi Earth!"};
 
+app.use('/graphql', graphqlHTTP({
+    schema: schema, 
+    rootValue: root, 
+    graphiql: true,
+}));
 // Add port server listens to
-app.listen(3000, () => console.log('Running server on port localhost:8080/graphql'));
+app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
 
