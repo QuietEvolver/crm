df487d4ab59057d8553343435f29583780aae2e2
VW <vera.weikel@gmail.com>
2023-10-04 15:28:51 -0700

    Define/pass Widgets db through model cnxn passing schema.

--- dbConnectors.js
+++ dbConnectors.js
@@ -2,4 +2,29 @@ import mongoose, { mongo } from "mongoose";
 
 // Mongo connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/widgets');
+mongoose.connect('mongodb://localhost/widgets', {
+    useNewUrlParser: true
+});
+
+const widgetSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+    }, 
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    soldout: {
+        type: String
+    },
+    inventory: {
+        type: String
+    }, 
+    stores: {
+        type: Array
+    }
+}); 
+
+const Widgets = mongoose.model('widgets');
